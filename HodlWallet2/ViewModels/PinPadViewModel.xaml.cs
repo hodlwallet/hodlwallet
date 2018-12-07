c6855efbe3d102978cf57d18d3229639407dc7bd
edgarnet <eaprmail@gmail.com>
2018-12-07 17:25:05 -0500

    Create PinPad

---
+++ HodlWallet2/ViewModels/PinPadViewModel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.ViewModels
+{
+    public partial class PinPadViewModel : ContentPage
+    {
+        public PinPadViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
