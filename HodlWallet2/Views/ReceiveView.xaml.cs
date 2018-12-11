eada1e3bf3adf0cd47eb96564ab2c393d389e50c
Igor Guerrero <igfgt1@gmail.com>
2018-12-11 09:06:19 -0500

    Trying different things to show the qr code

--- HodlWallet2/Views/ReceiveView.xaml.cs
+++ HodlWallet2/Views/ReceiveView.xaml.cs
@@ -15,6 +15,13 @@ namespace HodlWallet2.Views
             InitializeComponent();
 
             BindingContext = viewModel;
+
+            _QrCode.Source = viewModel.BarcodeImageSource;
+            _QrCode.Aspect = Aspect.Fill;
+
+            _QrCode.BindingContext = viewModel.BarcodeImage;
+
+            //Content = new ContentView { Content = viewModel.BarcodeImage };
         }
 
         public ReceiveViewModel ViewModel { get => BindingContext as ReceiveViewModel; }
