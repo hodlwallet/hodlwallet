47888fc144192f822b21bc9691b17eec6dcbec23
edgarnet <eaprmail@gmail.com>
2018-12-13 09:57:34 -0500

    Add PinPad Button Functionality

--- HodlWallet2/ViewModels/PinPadViewModel.xaml.cs
+++ HodlWallet2/ViewModels/PinPadViewModel.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
 
 using Xamarin.Forms;
+
+using HodlWallet2.Locale;
 
 namespace HodlWallet2.ViewModels
 {
@@ -11,5 +15,52 @@ namespace HodlWallet2.ViewModels
         {
             InitializeComponent();
         }
+
+    }
+
+    public class PinPad : INotifyPropertyChanged
+    {
+        private List<int> Pin = new List<int>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PinPad()
+        {
+            BackspaceCommand = new Command(
+                execute: () =>
+                {
+                    if (Pin.Count - 1 >= 0)
+                    {
+                        Pin.RemoveAt(Pin.Count - 1);
+                        RefreshCanExecutes();
+                    }
+                });
+
+            DigitCommand = new Command<string>(
+                execute: (string arg) =>
+                {
+                    if (Pin.Count < 6)
+                    {
+                        Pin.Add(int.Parse(arg));
+                        RefreshCanExecutes();
+                    }
+                    else
+                    {
+                        // Store Pin
+                        Pin.Clear();
+                        RefreshCanExecutes();
+                    }
+                });
+        }
+
+        void RefreshCanExecutes()
+        {
+            ((Command)BackspaceCommand).ChangeCanExecute();
+            ((Command)DigitCommand).ChangeCanExecute();
+        }
+
+        public ICommand BackspaceCommand { private set; get; }
+
+        public ICommand DigitCommand { private set; get; }
     }
 }
