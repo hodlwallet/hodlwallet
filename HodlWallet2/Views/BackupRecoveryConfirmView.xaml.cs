34cc5d7b9f2f69a093acfaca7d938f58798982d1
edgarnet <eaprmail@gmail.com>
2018-12-17 18:51:16 -0500

    Add Views for Recovery Key

---
+++ HodlWallet2/Views/BackupRecoveryConfirmView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.Views
+{
+    public partial class BackupRecoveryConfirmView : ContentPage
+    {
+        public BackupRecoveryConfirmView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HodlWallet2/Views/SeedView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HodlWallet2.Locale;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.Views
+{
+    public partial class SeedView : ContentPage
+    {
+        public SeedView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        private void SetLabels()
+        {
+            Title.Text = LocaleResources.Seed_title.ToUpper();
+            Subheader.Text = LocaleResources.Seed_subheader;
+            NextButton.Text = LocaleResources.Seed_next.ToUpper();
+        }
+    }
+}
