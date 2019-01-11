5ba66cd9a5eeb84f1201284a9f033bcc7b72bdf4
edgarnet <eaprmail@gmail.com>
2019-01-11 15:49:10 -0500

    Recover Wallet View Model

---
+++ HodlWallet2/ViewModels/RecoverSeedEntryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.ViewModels
+{
+    public class RecoverSeedEntryViewModel : ContentView
+    {
+        public RecoverSeedEntryViewModel()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
