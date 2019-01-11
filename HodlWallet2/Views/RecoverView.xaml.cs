481fa3644c5abc516f8a251d7d26ad8249ca3b99
edgarnet <eaprmail@gmail.com>
2019-01-11 12:42:58 -0500

    Add Onboard Recover Views

---
+++ HodlWallet2/Views/RecoverView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HodlWallet2.Locale;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.Views
+{
+    public partial class RecoverView : ContentPage
+    {
+        public RecoverView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        private void SetLabels()
+        {
+            Title.Text = LocaleResources.Recover_title;
+            Subheader.Text = LocaleResources.Recover_subheader;
+            Button.Text = LocaleResources.Seed_next;
+        }
+    }
+}
