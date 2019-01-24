e0e67ee8a74bae9ecce49f605be17aaf3e157c32
edgarnet <eaprmail@gmail.com>
2019-01-23 23:52:17 -0500

    [WIP] Update Backup Recovery Views

--- HodlWallet2/Views/BackupRecoveryWordView.xaml.cs
+++ HodlWallet2/Views/BackupRecoveryWordView.xaml.cs
@@ -2,26 +2,52 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Serilog;
 
 using HodlWallet2.Locale;
+using HodlWallet2.Utils;
 
 namespace HodlWallet2.Views
 {
     public partial class BackupRecoveryWordView : ContentPage
     {
-        public BackupRecoveryWordView(int position, string word)
+        int Position;
+        string[] Words;
+        Wallet _Wallet;
+        ILogger _Logger;
+
+        public BackupRecoveryWordView(int position, string[] words)
         {
+            _Wallet = Wallet.Instance;
+            _Logger = _Wallet.Logger;
             InitializeComponent();
-            SetLabels(position, word);
+            Position = position;
+            Words = words;
+            SetLabels();
         }
 
-        private void SetLabels(int position, string word)
+        private void SetLabels()
         {
             Title.Text = LocaleResources.Backup_title;
             Header.Text = LocaleResources.Backup_wordheader;
-            Position.Text = position + " of 12";
-            Word.Text = word;
+            Index.Text = Position + " of 12";
+            Word.Text = Words[Position - 1];
             Next.Text = LocaleResources.Seed_next;
+        }
+
+        private void BackupWord_Clicked(object sender, EventArgs e)
+        {
+            if (Position < 12)
+            {
+                Position++;
+                Navigation.PushAsync(new BackupRecoveryWordView(Position, Words));
+                _Logger.Information("Backup button clicked.");
+            }
+            else
+            {
+                Navigation.PushAsync(new BackupRecoveryConfirmView());
+                _Logger.Information("Backup recovery confirmation initiated.");
+            }
         }
     }
 }
--- HodlWallet2/Views/BackupView.xaml.cs
+++ HodlWallet2/Views/BackupView.xaml.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using HodlWallet2.Locale;
-
 using Xamarin.Forms;
+using Serilog;
+
+using HodlWallet2.Locale;
+using HodlWallet2.ViewModels;
 
 namespace HodlWallet2.Views
 {
     public partial class BackupView : ContentPage
     {
+        Wallet _Wallet;
+        ILogger _Logger;
+        string[] Words = { "zebra", "blame", "wink", "cactus", "hungry", "often", "uniform", "minute", "broom", "weapon", "venture", "creek" }; /* WIP until Wallet.Instance isses are fixed */
+
         public BackupView()
         {
+            _Wallet = Wallet.Instance;
+            _Logger = _Wallet.Logger;
+
+            if (/*SecureStorageProvider.HasMnemonic() == false*/ true)
+            {
+                // SecureStorageProvider.SetMnemonic(_Wallet.WalletManager.CreateWallet("hodl", SecureStorageProvider.GetPassword(), null, "english", 12).ToString());
+            }
             InitializeComponent();
             SetLabels();
         }
@@ -20,6 +33,12 @@ namespace HodlWallet2.Views
             Title.Text = LocaleResources.Backup_title;
             Subheader.Text = LocaleResources.Backup_subheader;
             Button.Text = LocaleResources.Backup_button;
+        }
+
+        private void BackupButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BackupRecoveryWordView(1, Words));
+            _Logger.Information("Backup button clicked.");
         }
     }
 }
