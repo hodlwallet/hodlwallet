afa8a692fc2478dd95816057dbb3a674d0a29d77
edgarnet <eaprmail@gmail.com>
2019-01-28 11:51:58 -0500

    Setup Recover View Navigation

---
+++ HodlWallet2/Views/RecoverView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using HodlWallet2.Locale;
+
+namespace HodlWallet2.Views
+{
+    public partial class RecoverView : ContentPage
+    {
+        public RecoverView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        private void SetLabels()
+        {
+            Title.Text = LocaleResources.Recover_title;
+            Header.Text = LocaleResources.Recover_header;
+            Next.Text = LocaleResources.Recover_next;
+        }
+    }
+}
