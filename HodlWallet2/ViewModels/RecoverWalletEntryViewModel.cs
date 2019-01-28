b65daf8469b4b02c3cab3e939539d4feb5dc84c8
edgarnet <eaprmail@gmail.com>
2019-01-28 12:16:19 -0500

    Add Recover Entry View Model

---
+++ HodlWallet2/ViewModels/RecoverWalletEntryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.ViewModels
+{
+    public class RecoverWalletEntryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RecoverWalletEntryViewModel()
+        {
+
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        void RefreshCanExecutes()
+        {
+            ((Command)BackspaceCommand).ChangeCanExecute();
+            ((Command)DigitCommand).ChangeCanExecute();
+        }
+
+        public ICommand BackspaceCommand { private set; get; }
+
+        public ICommand DigitCommand { private set; get; }
+    }
+}
+
--- HodlWallet2/Views/RecoverView.xaml.cs
+++ HodlWallet2/Views/RecoverView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Serilog;
 
 using HodlWallet2.Locale;
 
@@ -9,8 +10,14 @@ namespace HodlWallet2.Views
 {
     public partial class RecoverView : ContentPage
     {
+        Wallet _Wallet;
+        ILogger _Logger;
+
         public RecoverView()
         {
+            _Wallet = Wallet.Instance;
+            _Logger = _Wallet.Logger;
+
             InitializeComponent();
             SetLabels();
         }
@@ -20,6 +27,12 @@ namespace HodlWallet2.Views
             Title.Text = LocaleResources.Recover_title;
             Header.Text = LocaleResources.Recover_header;
             Next.Text = LocaleResources.Recover_next;
+        }
+
+        private void RecoverNext_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RecoverWalletEntryView());
+            _Logger.Information("Recover button clicked.");
         }
     }
 }
