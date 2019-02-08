5939eb7cf09305f9160f47c0937b37969c1e62a4
edgarnet <eaprmail@gmail.com>
2019-02-08 13:13:21 -0500

    Add View Model Binding Context

--- HodlWallet2/Views/LoginView.xaml.cs
+++ HodlWallet2/Views/LoginView.xaml.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 
 using HodlWallet2.Locale;
+using HodlWallet2.ViewModels;
 
 namespace HodlWallet2.Views
 {
     public partial class LoginView : ContentPage
     {
+        public LoginViewModel ViewModel { get { return BindingContext as LoginViewModel; } }
+
         public LoginView()
         {
             InitializeComponent();
