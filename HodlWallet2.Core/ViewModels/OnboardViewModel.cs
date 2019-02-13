a00211d4803c1a8c1d9469b2a1ce06496b03c0bf
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-12 19:15:34 -0500

    Added Tag property and method binding for navigation.

--- HodlWallet2.Core/ViewModels/OnboardViewModel.cs
+++ HodlWallet2.Core/ViewModels/OnboardViewModel.cs
@@ -1,13 +1,32 @@
+using System.Threading.Tasks;
+using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
 
 namespace HodlWallet2.Core.ViewModels
 {
     public class OnboardViewModel : BaseViewModel
-    {
+    {        
+        public IMvxAsyncCommand<string> NavigateToViewCommand { get; private set; } 
+        
         public OnboardViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) 
             : base(logProvider, navigationService)
         {
+            NavigateToViewCommand = new MvxAsyncCommand<string>(NavigateToView);
+        }
+
+        private async Task NavigateToView(string option)
+        {
+            switch (option)
+            {
+                case "Create":
+                    //TODO: Add PinViewModel
+                    //await NavigationService.Navigate<PinViewModel>();
+                    break;
+                case "Recover":
+                    await NavigationService.Navigate<RecoverViewModel>();
+                    break;
+            }
         }
     }
 }
