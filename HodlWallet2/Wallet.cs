10c036012cbe95239d2650e83e3c3f9e9855d4fd
Igor Guerrero <igorgue@protonmail.com>
2019-02-19 11:47:52 -0500

    Make user agent include liviano version as well

--- HodlWallet2/Wallet.cs
+++ HodlWallet2/Wallet.cs
@@ -32,6 +32,8 @@ namespace HodlWallet2
 
         public const string DEFAULT_NETWORK = "main";
 
+        public static readonly string USER_AGENT = $"{Liviano.Version.UserAgent}/hodlwallet:2.0/";
+
         private static Wallet instance = null;
 
         private static readonly object _SingletonLock = new object();
@@ -296,7 +298,7 @@ namespace HodlWallet2
             _ConParams.TemplateBehaviors.Add(new PartialChainBehavior(_Chain, _Network) { CanRespondToGetHeaders = false, SkipPoWCheck = true });
             _ConParams.TemplateBehaviors.Add(_WalletSyncManagerBehavior);
 
-            _ConParams.UserAgent = "/hodlwallet:2.0/";
+            _ConParams.UserAgent = USER_AGENT;
 
             _NodesGroup = new NodesGroup(_Network, _ConParams, new NodeRequirement()
             {
