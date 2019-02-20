5787cd9347dd192a04cf6c2c33056e064368f287
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-19 19:40:27 -0500

    Navigation for RecoverView is now handled at the ViewModel

--- HodlWallet2/Views/OnboardView.xaml.cs
+++ HodlWallet2/Views/OnboardView.xaml.cs
@@ -32,7 +32,7 @@ namespace HodlWallet2.Views
 
         private void RecoverButton_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new RecoverView());
+            //Navigation.PushAsync(new RecoverView());
             _Logger.Information("Recover button clicked.");
         }
     }
