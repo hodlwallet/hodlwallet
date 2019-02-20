da13f1549dbd3772b5533ed21f676bae742a101e
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-19 19:39:11 -0500

    Added Behavior logic

--- HodlWallet2/Behaviors/PaintBoxViewBehavior.cs
+++ HodlWallet2/Behaviors/PaintBoxViewBehavior.cs
@@ -1,9 +1,25 @@
+using System;
 using Xamarin.Forms;
 
 namespace HodlWallet2.Behaviors
 {
     public class PaintBoxViewBehavior : Behavior<Button>
     {
-        
+        protected override void OnAttachedTo(Button bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.Clicked += BindableOnClicked;
+        }
+
+        protected override void OnDetachingFrom(Button bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.Clicked -= BindableOnClicked;
+        }
+
+        private void BindableOnClicked(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
