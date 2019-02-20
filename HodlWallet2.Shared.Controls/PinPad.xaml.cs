341e0ac8eefbd9aae06040642a05fa97a02ee8b6
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-19 20:27:36 -0500

    Added new project for Shared UI Controls Added text bindable properties for PinPad control.

---
+++ HodlWallet2.Shared.Controls/PinPad.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HodlWallet2.Shared.Controls
+{
+    public partial class PinPad : ContentView
+    {
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.CreateAttached(
+                "TitleText", 
+                typeof(string), 
+                typeof(PinPad), 
+                default(string)
+            );
+
+        public string TitleText
+        {
+            get => (string) GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        
+        public static readonly BindableProperty HeaderProperty =
+            BindableProperty.CreateAttached(
+                "HeaderText", 
+                typeof(string), 
+                typeof(PinPad), 
+                default(string)
+            );
+
+        public string HeaderText
+        {
+            get => (string) GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+        
+        public static readonly BindableProperty WarningProperty =
+            BindableProperty.CreateAttached(
+                "WarningText", 
+                typeof(string), 
+                typeof(PinPad), 
+                default(string)
+            );
+
+        public string WarningText
+        {
+            get => (string) GetValue(WarningProperty);
+            set => SetValue(WarningProperty, value);
+        }
+
+        public PinPad()
+        {
+            InitializeComponent();
+            
+            lblTitle.SetBinding(Label.TextProperty, new Binding(nameof(TitleText), source: this));
+            lblHeader.SetBinding(Label.TextProperty, new Binding(nameof(HeaderText), source: this));
+            lblWarning.SetBinding(Label.TextProperty, new Binding(nameof(WarningText), source: this));
+        }
+    }
+}
---
+++ HodlWallet2.Shared.Controls/Utils/Tags.cs
@@ -0,0 +1,23 @@
+using Xamarin.Forms;
+
+namespace HodlWallet2.Shared.Controls.Utils
+{
+    public class Tags
+    {
+        public static readonly BindableProperty TagProperty = BindableProperty.Create(
+            "Tag", 
+            typeof(string), 
+            typeof(Tags), 
+            null);
+
+        public static string GetTag(BindableObject bindable)
+        {
+            return (string)bindable.GetValue(TagProperty);
+        }
+
+        public static void SetTag(BindableObject bindable, string value)
+        {
+        }
+
+    }
+}
