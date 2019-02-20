a2664f8da95aa71406954e74b8ce12c6db3e28dd
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-19 19:38:20 -0500

    Created SetPinViewModel

--- HodlWallet2.Core/ViewModels/SetPinViewModel.cs
+++ HodlWallet2.Core/ViewModels/SetPinViewModel.cs
@@ -1,7 +1,15 @@
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class SetPinViewModel
+    public class SetPinViewModel : BaseViewModel
     {
+        protected SetPinViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) 
+            : base(logProvider, navigationService)
+        {
+        }
+        
         
     }
 }
