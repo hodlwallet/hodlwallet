078e39b06bcccd0ebceb66bcbca26f177540eb1a
Abraham Chacon Landois <maharbazster@gmail.com>
2019-02-28 19:15:47 -0600

    RecoverWalletEntry view converted to its MvvmCross version. Fixed StaticResource issue. Values pending.

--- HodlWallet2/Views/RecoverWalletEntryView.xaml.cs
+++ HodlWallet2/Views/RecoverWalletEntryView.xaml.cs
@@ -6,33 +6,29 @@ using Xamarin.Forms;
 using Serilog;
 
 using HodlWallet2.Locale;
-using HodlWallet2.ViewModels;
+using HodlWallet2.Core.ViewModels;
+using MvvmCross.Forms.Views;
 
 namespace HodlWallet2.Views
 {
-    public partial class RecoverWalletEntryView : ContentPage
+    public partial class RecoverWalletEntryView : MvxContentPage<RecoverWalletEntryViewModel>
     {
         Wallet _Wallet;
         ILogger _Logger;
 
         public RecoverWalletEntryView()
         {
-
-        }
-
-        public RecoverWalletEntryView(RecoverWalletEntryViewModel viewModel)
-        {
             InitializeComponent();
-            _Wallet = Wallet.Instance;
-            _Logger = _Wallet.Logger;
-            viewModel._EntryGrid = EntryGrid;
-            SetLabels();
+            //_Wallet = Wallet.Instance;
+            //_Logger = _Wallet.Logger;
+            //viewModel._EntryGrid = EntryGrid;
+            //SetLabels();
         }
 
         private void SetLabels()
         {
-            Title.Text = LocaleResources.Recover_title;
-            Header.Text = LocaleResources.Recover_entryHeader;
+//            Title.Text = LocaleResources.Recover_title;
+//            Header.Text = LocaleResources.Recover_entryHeader;
         }
 
         private void Entry_Completed(object sender, EventArgs e)
