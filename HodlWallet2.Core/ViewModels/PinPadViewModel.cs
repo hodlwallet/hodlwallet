02a58eef9c728e5b216a7c7ff92addfb40737db6
Abraham Chacon Landois <maharbazster@gmail.com>
2019-03-02 14:47:43 -0600

    Added temporal properties for Custom Control.

--- HodlWallet2.Core/ViewModels/PinPadViewModel.cs
+++ HodlWallet2.Core/ViewModels/PinPadViewModel.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
 
@@ -5,9 +7,20 @@ namespace HodlWallet2.Core.ViewModels
 {
     public class PinPadViewModel : BaseViewModel
     {
+        public IMvxCommand<string> SuccessCommand { get; private set; }
+        
+        //TODO: Localize properties
+        public string PinPadTitle => "This Title is coming from the ViewModel";
+
+        public string PinPadHeader => "This Header is coming from the ViewModel";
+
+        public string PinPadWarning => "This Warning is coming from the ViewModel";
+
         public PinPadViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) 
             : base(logProvider, navigationService)
         {
+            //TODO: Change Action.
+            SuccessCommand = new MvxCommand<string>(pin => Debug.WriteLine($"PIN Saved: {pin}"));
         }
     }
 }
