cce2d73b8e248f6c535f84aeb0be497c31d1e17a
edgarnet <eaprmail@gmail.com>
2019-03-04 17:42:31 -0500

    [WIP] Start Menu View UI

--- HodlWallet2/Views/MenuView.xaml.cs
+++ HodlWallet2/Views/MenuView.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
+using HodlWallet2.Locale;
+
 namespace HodlWallet2.Views
 {
     public partial class MenuView : ContentPage
@@ -10,6 +12,21 @@ namespace HodlWallet2.Views
         public MenuView()
         {
             InitializeComponent();
+            SetLabels();
+        }
+
+        public async void OnCloseTapped(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private void SetLabels()
+        {
+            MenuTitle.Text = LocaleResources.Menu_title;
+            Security.Text = LocaleResources.Menu_security;
+            Knowledge.Text = LocaleResources.Menu_knowledge;
+            Settings.Text = LocaleResources.Menu_settings;
+            LockWallet.Text = LocaleResources.Menu_lock;
         }
     }
 }
