6e2606d27060e7b853f53f40778819de16283604
edgarnet <eaprmail@gmail.com>
2019-03-06 19:16:23 -0500

    Add Icons and Views for Security Center

---
+++ HodlWallet2/ViewModels/SecurityCenterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.ViewModels
+{
+    public class SecurityCenterViewModel : ContentPage
+    {
+        public SecurityCenterViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
