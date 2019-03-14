7ce3909edc6a6553980e7af27cbb036ac1f4011f
edgarnet <eaprmail@gmail.com>
2019-03-14 16:25:46 -0400

    Add Mnemonic Button Functionality

--- HodlWallet2/Views/SecurityCenterView.xaml.cs
+++ HodlWallet2/Views/SecurityCenterView.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms;
 
 using HodlWallet2.ViewModels;
 using HodlWallet2.Locale;
+using HodlWallet2.Utils;
 
 namespace HodlWallet2.Views
 {
@@ -49,6 +50,12 @@ namespace HodlWallet2.Views
         public async void OnFingerprintTapped(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new FingerprintAuthView());
+        }
+
+        public async void OnMnemonicTapped(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+            await App.Current.MainPage.Navigation.PushAsync(new CustomNavigationPage(new BackupView()));
         }
 
         protected override void OnAppearing()
