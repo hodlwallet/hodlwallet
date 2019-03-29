aeda67e640db4597625ff604694b85c9d10cc00c
Igor Guerrero <igfgt1@gmail.com>
2019-03-29 11:17:42 -0400

    Move Wallet.cs to WalletService.cs and start it as a service

---
+++ HodlWallet2.Core/Services/WalletStorageProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Liviano;
+
+namespace HodlWallet2.Core
+{
+    public class WalletStorageProvider : FileSystemStorageProvider
+    {
+        public WalletStorageProvider(string id = null) : base(id, Environment.GetFolderPath(Environment.SpecialFolder.Personal))
+        {
+        }
+    }
+}
