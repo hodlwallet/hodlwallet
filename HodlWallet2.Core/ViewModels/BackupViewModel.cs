4fee6d34e402fd4813d925e0a95f9aeed3b7bbce
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-13 13:08:40 -0400

    Implemented BackupViewModel

--- HodlWallet2.Core/ViewModels/BackupViewModel.cs
+++ HodlWallet2.Core/ViewModels/BackupViewModel.cs
@@ -1,7 +1,22 @@
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class BackupViewModel
+    public class BackupViewModel : BaseViewModel
     {
+        public IMvxAsyncCommand<string> NavigateToViewCommand { get; private set; }
         
+        public BackupViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+            NavigateToViewCommand = new MvxAsyncCommand<string>(NavigateToView);
+        }
+
+        private async Task NavigateToView(string arg)
+        {
+            await NavigationService.Navigate<BackupRecoveryWordViewModel>();
+        }
     }
 }
