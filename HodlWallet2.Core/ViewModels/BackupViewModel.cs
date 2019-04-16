0ad56c5cfdd354476dfdff39d29be9a9e8a203fb
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-15 20:06:22 -0400

    Added string properties and command for BackupViewModel

--- HodlWallet2.Core/ViewModels/BackupViewModel.cs
+++ HodlWallet2.Core/ViewModels/BackupViewModel.cs
@@ -1,21 +1,34 @@
 using System.Threading.Tasks;
+using HodlWallet2.Core.Services;
+using HodlWallet2.Core.Utils;
 using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
+using Serilog;
 
 namespace HodlWallet2.Core.ViewModels
 {
     public class BackupViewModel : BaseViewModel
     {
-        public IMvxAsyncCommand<string> NavigateToViewCommand { get; private set; }
+        //TODO: Add localization strings
+        public string BackupTitle => "Backup Recovery Key";
+
+        public string HeaderText =>
+            "Your backup recovery key is the only way to restore your wallet if your phone is lost, stolen, broken or upgraded.";
+
+        public string SubheaderText =>
+            "We will show you a list of words to write down on a piece of paper and keep safe.";
+        public string ButtonText => "Write Down Backup Recovery Key";
+        public IMvxAsyncCommand<string> WriteDownWordsCommand { get; private set; }
         
         public BackupViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
-            NavigateToViewCommand = new MvxAsyncCommand<string>(NavigateToView);
+            WriteDownWordsCommand = new MvxAsyncCommand<string>(NavigateToView);
         }
 
         private async Task NavigateToView(string arg)
         {
+            //TODO: WIP - Add logic for BAckupRecoveryWordViewModel.
             await NavigationService.Navigate<BackupRecoveryWordViewModel>();
         }
     }
