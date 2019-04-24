a2a927e1e4393cc66e1113b40d20de508abcce90
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-24 18:50:41 -0400

    Bindings update on view

--- HodlWallet2/Views/BackupRecoveryConfirmView.xaml.cs
+++ HodlWallet2/Views/BackupRecoveryConfirmView.xaml.cs
@@ -1,37 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HodlWallet2.Core.ViewModels;
 using Xamarin.Forms;
 
 using HodlWallet2.Locale;
 using HodlWallet2.ViewModels;
+using MvvmCross.Forms.Views;
 
 namespace HodlWallet2.Views
 {
-    public partial class BackupRecoveryConfirmView : ContentPage
+    public partial class BackupRecoveryConfirmView : MvxContentPage<BackupRecoveryConfirmViewModel>
     {
-        public BackupConfirmViewModel ViewModel { get => BindingContext as BackupConfirmViewModel; }
-        private BackupConfirmViewModel _ViewModel;
-
-        public BackupRecoveryConfirmView(BackupConfirmViewModel viewModel)
-        {
-            InitializeComponent();
-            SetLabels();
-            _ViewModel = viewModel;
-            BindingContext = _ViewModel;
-        }
-
         public BackupRecoveryConfirmView()
         {
             InitializeComponent();
-            SetLabels();
         }
-
-        private void SetLabels()
-        {
-            Title = LocaleResources.Backup_title;
-            Header.Text = LocaleResources.ConfirmBackup_header;
-        }
-
     }
 }
