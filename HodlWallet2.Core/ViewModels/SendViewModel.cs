84ed0a610c37b19319cd655a1d3c7968b64dbeb3
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-25 20:45:20 -0400

    Modified SendViewModel.

--- HodlWallet2.Core/ViewModels/SendViewModel.cs
+++ HodlWallet2.Core/ViewModels/SendViewModel.cs
@@ -1,7 +1,12 @@
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class SendViewModel
+    public class SendViewModel : BaseViewModel
     {
-        
+        public SendViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+        }
     }
 }
