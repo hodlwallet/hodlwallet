c930e75421eb3686990bad7384c0c33cc7b17cef
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-25 20:39:08 -0400

    Implemented DashboardViewModel.

--- HodlWallet2.Core/ViewModels/DashboardViewModel.cs
+++ HodlWallet2.Core/ViewModels/DashboardViewModel.cs
@@ -1,7 +1,46 @@
+using HodlWallet2.Core.Interfaces;
+using HodlWallet2.Core.Services;
+using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class DashboardViewModel
+    public class DashboardViewModel : BaseViewModel
     {
+        private readonly IWalletService _walletService;
+
+        public string SendText => "Send";
+        public string ReceiveText => "Receive";
         
+        public MvxCommand NavigateToSendViewCommand { get; private set; }
+        public MvxCommand NavigateToReceiveViewCommand { get; private set; }
+        public MvxCommand NavigateToMenuViewCommand { get; private set; }
+        
+        public DashboardViewModel(
+            IMvxLogProvider logProvider, 
+            IMvxNavigationService navigationService,
+            IWalletService walletService) : base(logProvider, navigationService)
+        {
+            _walletService = walletService;
+            NavigateToSendViewCommand = new MvxCommand(NavigateToSendView);
+            NavigateToReceiveViewCommand = new MvxCommand(NavigateToReceiveView);
+            NavigateToMenuViewCommand = new MvxCommand(NavigateToMenuView);
+        }
+
+        private void NavigateToMenuView()
+        {
+            NavigationService.Navigate<MenuViewModel>();
+        }
+
+        private void NavigateToReceiveView()
+        {
+            NavigationService.Navigate<ReceiveViewModel>();
+        }
+
+        private void NavigateToSendView()
+        {
+            NavigationService.Navigate<SendViewModel>();
+        }
     }
 }
