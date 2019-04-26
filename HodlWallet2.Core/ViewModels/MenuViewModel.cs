9f2aed2a9b220f57d909e0d7834641ef2ea5581e
Abraham Chacon Landois <maharbazster@gmail.com>
2019-04-25 20:40:21 -0400

    Modified MenuViewModel

--- HodlWallet2.Core/ViewModels/MenuViewModel.cs
+++ HodlWallet2.Core/ViewModels/MenuViewModel.cs
@@ -1,7 +1,14 @@
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class MenuViewModel
+    public class MenuViewModel : BaseViewModel
     {
-        
+        public MenuViewModel(
+            IMvxLogProvider logProvider,
+            IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+        }
     }
 }
