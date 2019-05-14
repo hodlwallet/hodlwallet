4a4e54898cc3547fc9d6491998485d06b8c753b0
Abraham Chacon Landois <maharbazster@gmail.com>
2019-05-14 00:37:39 -0400

    Adapted MenuView to MvvmCross

--- HodlWallet2/Views/MenuView.xaml.cs
+++ HodlWallet2/Views/MenuView.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HodlWallet2.Core.ViewModels;
 using Xamarin.Forms;
 
 using HodlWallet2.Locale;
 using HodlWallet2.Utils;
 using HodlWallet2.ViewModels;
+using MvvmCross.Commands;
+using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
 
 namespace HodlWallet2.Views
 {
-    public partial class MenuView : ContentPage
+    [MvxModalPresentation]
+    public partial class MenuView : MvxContentPage<MenuViewModel>
     {
         public MenuView()
         {
@@ -19,22 +23,12 @@ namespace HodlWallet2.Views
 
         private void SetLabels()
         {
+            //TODO: Move localization strings to Core
             MenuTitle.Text = LocaleResources.Menu_title;
             Security.Text = LocaleResources.Menu_security;
             Knowledge.Text = LocaleResources.Menu_knowledge;
             Settings.Text = LocaleResources.Menu_settings;
             LockWallet.Text = LocaleResources.Menu_lock;
-        }
-
-        public async void OnCloseTapped(object sender, EventArgs e)
-        {
-            await Navigation.PopModalAsync();
-        }
-
-        public async void OnSecurityClicked(object sender, EventArgs e)
-        {
-            await Navigation.PopModalAsync();
-            await App.Current.MainPage.Navigation.PushModalAsync(new CustomNavigationPage(new SecurityCenterView(new SecurityCenterViewModel())));
         }
     }
 }
