ad0d59b7118d11e53e1295e1d09d4c6d85c590d0
Abraham Chacon Landois <maharbazster@gmail.com>
2019-05-14 20:08:54 -0400

    Moved code from old view model to new Core LoginViewModel

--- HodlWallet2.Core/ViewModels/LoginViewModel.cs
+++ HodlWallet2.Core/ViewModels/LoginViewModel.cs
@@ -1,7 +1,58 @@
+using System.Collections.Generic;
+using System.Drawing;
+using HodlWallet2.Core.Utils;
+using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
 namespace HodlWallet2.Core.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
-        
+        private List<int> _pin;
+        public MvxCommand<int> DigitCommand { get; private set; }
+        public MvxCommand BackspaceCommand { get; private set; }
+        protected LoginViewModel(
+            IMvxLogProvider logProvider, 
+            IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+            _pin = new List<int>();
+            DigitCommand = new MvxCommand<int>(DigitTapped);
+            BackspaceCommand = new MvxCommand(BackspaceTapped);
+        }
+
+        private void BackspaceTapped()
+        {
+            if (_pin.Count - 1 >= 0)
+            {
+                _pin.RemoveAt(_pin.Count - 1);
+            }
+        }
+
+        private async void DigitTapped(int arg)
+        {
+            if (_pin.Count < 6)
+            {
+                _pin.Add(arg);
+                //TODO: Set color to specific digit.
+                if (_pin.Count == 6)
+                {
+                    //TODO: Reset colors of all digits.
+
+                    if (SecureStorageProvider.HasPassword() == false)
+                    {
+                        // TODO: Throw exception
+                        _pin.Clear();
+                        return;
+                    }
+                    if (SecureStorageProvider.GetPassword() == string.Join(string.Empty, _pin.ToArray()))
+                    {
+                        _pin.Clear();
+                        await NavigationService.Navigate<DashboardViewModel>();
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
