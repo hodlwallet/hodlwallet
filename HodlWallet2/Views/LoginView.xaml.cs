867ae2640ddd1fadf6d4249b260a39241760f565
Abraham Chacon Landois <maharbazster@gmail.com>
2019-05-14 20:09:22 -0400

    Adapted LoginView for MvvmCross

--- HodlWallet2/Views/LoginView.xaml.cs
+++ HodlWallet2/Views/LoginView.xaml.cs
@@ -1,23 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HodlWallet2.Core.ViewModels;
 using Xamarin.Forms;
 
 using HodlWallet2.Locale;
-using HodlWallet2.ViewModels;
+using MvvmCross.Forms.Views;
 
 namespace HodlWallet2.Views
 {
-    public partial class LoginView : ContentPage
+    public partial class LoginView : MvxContentPage<LoginViewModel>
     {
-        public LoginViewModel ViewModel { get { return BindingContext as LoginViewModel; } }
-
         public LoginView(LoginViewModel viewModel)
         {
             InitializeComponent();
             SetLabel();
-            viewModel._Navigation = Navigation;
-            BindingContext = viewModel;
         }
 
         private void SetLabel()
