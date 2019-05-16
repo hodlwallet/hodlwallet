a70032e88f6334f6ab4ae03339865ca6fae80d51
Abraham Chacon Landois <maharbazster@gmail.com>
2019-05-15 23:07:57 -0400

    Added implementation to new viewmodel

--- HodlWallet2.Core/ViewModels/SecurityCenterViewModel.cs
+++ HodlWallet2.Core/ViewModels/SecurityCenterViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using HodlWallet2.Core.Utils;
+using MvvmCross.Commands;
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
@@ -18,6 +19,15 @@ namespace HodlWallet2.Core.ViewModels
         private string _pinCheck;
         private string _fingerprintCheck;
         private string _mnemonicCheck;
+        
+        public MvxCommand CloseCommand { get; private set; }
+        public MvxCommand FaqCommand { get; private set; }
+        public MvxCommand PinCommand { get; private set; }
+        public MvxCommand FingerprintCommand { get; private set; }
+        public MvxCommand MnemonicCommand { get; private set; }
+        
+        //TODO: Add string properties here
+        
 
         public string PinCheck
         {
@@ -42,11 +52,41 @@ namespace HodlWallet2.Core.ViewModels
             IMvxNavigationService navigationService) 
             : base(logProvider, navigationService)
         {
+            CloseCommand = new MvxCommand(Close);
+            FaqCommand = new MvxCommand(FaqTapped);
+            PinCommand = new MvxCommand(PinTapped);
+            FingerprintCommand = new MvxCommand(FingerprintTapped);
+            MnemonicCommand = new MvxCommand(MnemonicTapped);
         }
 
-        public override void Prepare()
+        private async void MnemonicTapped()
         {
-            base.Prepare();
+            await NavigationService.Navigate<BackupViewModel>();
+        }
+
+        private void FingerprintTapped()
+        {
+            //TODO: Implement biometrics
+        }
+
+        private async void PinTapped()
+        {
+            await NavigationService.Navigate<PinPadViewModel>(); //TODO: Pass UPDATE parameter
+        }
+
+        private void FaqTapped()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ViewAppearing()
+        {
+            base.ViewAppearing();
             InitializeChecks();
         }
 
