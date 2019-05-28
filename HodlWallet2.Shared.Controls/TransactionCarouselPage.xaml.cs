50ae1b49ee86951684bb8cbc7e55e2d6959caa75
edgarnet <eaprmail@gmail.com>
2019-05-28 12:07:54 -0400

    Create Transaction Carousel Page

---
+++ HodlWallet2.Shared.Controls/TransactionCarouselPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.Shared.Controls
+{
+    public partial class TransactionCarouselPage : CarouselPage
+    {
+        public TransactionCarouselPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
