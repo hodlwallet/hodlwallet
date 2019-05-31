f57b3f65dc302bf5ccbe222327720d039bfa3978
edgarnet <eaprmail@gmail.com>
2019-05-31 18:56:24 -0400

    Add Transaction Collection as MVVM

---
+++ HodlWallet2.Core/ViewModels/TransactionCollectionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using HodlWallet2.Core.Interfaces;
+using HodlWallet2.Core.Services;
+using HodlWallet2.Core.Models;
+using HodlWallet2.Core.Utils;
+using Liviano.Models;
+using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using Newtonsoft.Json;
+
+namespace HodlWallet2.Core.ViewModels
+{
+    public class TransactionCollectionViewModel : BaseViewModel
+    {
+        public TransactionCollectionViewModel(
+            IMvxLogProvider logProvider,
+            IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+            // TODO
+        }
+    }
+}
