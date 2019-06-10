52dec7ec25e030a38637d499aa9787e58c454cbf
edgarnet <eaprmail@gmail.com>
2019-06-10 18:57:43 -0400

    Add Precio Service Functionality

--- HodlWallet2.Core/Services/PrecioService.cs
+++ HodlWallet2.Core/Services/PrecioService.cs
@@ -12,8 +12,18 @@ namespace HodlWallet2.Core.Services
 {
     public class PrecioService
     {
-        string url = "https://precio.bitstop.co";
+        readonly IPrecioService _PrecioApi;
+        string HOST = "https://precio.bitstop.co";
 
-        public PrecioService() { }
+        public PrecioService() 
+        {
+            _PrecioApi = RestService.For<IPrecioService>(HOST);
+        }
+
+        public async Task<List<CurrencyEntity>> GetRates() => await _PrecioApi.GetRates();
+
+        public async Task<List<CurrencyEntity>> GetTestnetRates() => await _PrecioApi.GetTestnetRates();
+
+        public async Task<FeeEntity> GetFees() => await _PrecioApi.GetFees();
     }
 }
