ff9b3d55dc72d0a16666bef5e69cf6b66a9cb529
edgarnet <eaprmail@gmail.com>
2019-06-10 13:08:50 -0400

    Create Precio Service

---
+++ HodlWallet2.Core/Services/PrecioService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using HodlWallet2.Core.Interfaces;
+using HodlWallet2.Core.Models;
+
+using Refit;
+using MvvmCross;
+
+namespace HodlWallet2.Core.Services
+{
+    public class PrecioService
+    {
+        string url = "https://precio.bitstop.co";
+
+        public PrecioService() { }
+    }
+}
