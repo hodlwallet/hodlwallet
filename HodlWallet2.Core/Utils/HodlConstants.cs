6cd709d2608dd4fc502946f9ccb049ae4fa7cd9c
edgarnet <eaprmail@gmail.com>
2019-06-13 03:53:05 -0400

    Add Hodl Core Constants

--- HodlWallet2.Core/Utils/HodlConstants.cs
+++ HodlWallet2.Core/Utils/HodlConstants.cs
@@ -1,8 +1,25 @@
 ﻿using System;
 namespace HodlWallet2.Core.Utils
 {
-    public static class CoreConstants
+    public static class HodlConstants
     {
-        public const string HostUrl = "https://precio.bitstop.co"
+        // Precio API
+        public const string HostUrl = "https://precio.bitstop.co";
+        public static readonly string[] CurrencyCodes = { "USD" };
+        public const string BtcUnit = "1 BTC = ";
+        public const string SatByteUnit = " sat/byte";
+
+        // Colors
+        public const string SyncGradientStartHex = "#DAAB28";
+        public const string GrayTextTintHex = "#A3A8AD";
+
+        // Localized Strings (TODO in MvvmCross)
+        public const string IsAvailable = "Available to spend";
+        public const string SentAmount = "Sent BTC ";
+        public const string ReceivedAmount = "Received BTC ";
+        public const string SentReceivedNull = "Send and Receive is NULL";
+
+        //Temporary Values
+        public const string Memo = "In Progress";
     }
 }
