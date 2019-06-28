c3014cc60462e01631007d5a41fc86ceeeeb6e79
edgarnet <eaprmail@gmail.com>
2019-06-28 15:17:19 -0400

    Add IPermissions Dependency Service

---
+++ HodlWallet2.Android/Renderers/AndroidPermissions.cs
@@ -0,0 +1,23 @@
+﻿using HodlWallet2.Core.Interfaces;
+using HodlWallet2.Droid.Renderers;
+using Xamarin.Forms;
+
+[assembly: Dependency (typeof (AndroidPermissions))]
+namespace HodlWallet2.Droid.Renderers
+{
+    public class AndroidPermissions : IPermissions
+    {
+        public bool HasCameraPermission()
+        {
+            var needsPermissionRequest = ZXing.Net.Mobile.Android.PermissionsHandler.NeedsPermissionRequest(MainActivity.Instance);
+
+            if (needsPermissionRequest)
+                ZXing.Net.Mobile.Android.PermissionsHandler.RequestPermissionsAsync(MainActivity.Instance);
+
+            if (!needsPermissionRequest)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ HodlWallet2.iOS/Renderers/iOSPermissions.cs
@@ -0,0 +1,15 @@
+﻿using HodlWallet2.Core.Interfaces;
+using HodlWallet2.iOS.Renderers;
+using Xamarin.Forms;
+
+[assembly: Dependency (typeof (iOSPermissions))]
+namespace HodlWallet2.iOS.Renderers
+{
+    public class iOSPermissions : IPermissions
+    {
+        public bool HasCameraPermission()
+        {
+            return true;
+        }
+    }
+}
