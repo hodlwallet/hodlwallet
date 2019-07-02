7bf437c36b6311e6e95d934cb54973be557f7add
edgarnet <eaprmail@gmail.com>
2019-07-02 18:57:12 -0400

    Increase Android Slider Thumb

--- HodlWallet2.Android/Renderers/FeeSlider.cs
+++ HodlWallet2.Android/Renderers/FeeSlider.cs
@@ -4,6 +4,9 @@ using HodlWallet2.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.Widget;
 
 [assembly: ExportRenderer(typeof(FeeSlider), typeof(FeeSliderRenderer))]
 namespace HodlWallet2.Droid.Renderers
@@ -17,6 +20,15 @@ namespace HodlWallet2.Droid.Renderers
         protected override void OnElementChanged(ElementChangedEventArgs<Slider> e)
         {
             base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                ShapeDrawable th = new ShapeDrawable(new OvalShape());
+                th.SetIntrinsicWidth(70);
+                th.SetIntrinsicHeight(70);
+                th.SetColorFilter(Android.Graphics.Color.White, Android.Graphics.PorterDuff.Mode.SrcOver);
+                Control.SetThumb(th);
+            }
         }
     }
 }
