e7dcc40e26666e013e1da532856ccc8dc6b2911c
Igor Guerrero <igorgue@protonmail.com>
2019-07-05 17:49:00 -0400

    Make the send view use the dialog the way it should with interactions

--- HodlWallet2/Views/SendView.xaml.cs
+++ HodlWallet2/Views/SendView.xaml.cs
@@ -15,47 +15,35 @@ using HodlWallet2.Locale;
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
 using HodlWallet2.Core.Services;
+using MvvmCross.ViewModels;
+using HodlWallet2.Core.Interactions;
+using MvvmCross.Base;
+using MvvmCross.Binding.BindingContext;
 
 namespace HodlWallet2.Views
 {
     [MvxModalPresentation]
     public partial class SendView : MvxContentPage<SendViewModel>
     {
+        IMvxInteraction<YesNoQuestion> _QuestionInteraction = new MvxInteraction<YesNoQuestion>();
+        public IMvxInteraction<YesNoQuestion> QuestionInteraction
+        {
+            get => _QuestionInteraction;
+
+            set
+            {
+                if (_QuestionInteraction != null)
+                    _QuestionInteraction.Requested -= QuestionInteraction_Requested;
+
+                _QuestionInteraction = value;
+                _QuestionInteraction.Requested += QuestionInteraction_Requested;
+            }
+        }
+
         public SendView()
         {
             InitializeComponent();
             SetLabels();
-        }
-
-        async Task ProcessClipboardContent()
-        {
-            string content = await Clipboard.GetTextAsync();
-
-            if (!ViewModel.IsBitcoinAddressOnClipboard(content)) return;
-            
-            string dialogContent = string.Format(
-                LocaleResources.Send_addressDetectedOnClipboardMessage,
-                content
-            );
-            string dialogTitle = LocaleResources.Send_addressDetectedOnClipboardTitle;
-            
-            bool answer = await DisplayAlert(
-                dialogTitle, dialogContent, "Yes", cancel: "No"
-            );
-
-            if (!answer) return;
-            
-            ViewModel.AddressToSendTo = content;
-        }
-
-        void SetLabels()
-        {
-            SendTitle.Text = LocaleResources.Send_title;
-            ToLabel.Text = LocaleResources.Send_to;
-            ScanLabel.Text = LocaleResources.Send_scan;
-            PasteLabel.Text = LocaleResources.Send_paste;
-            AmountLabel.Text = LocaleResources.Send_amount;
-            ISOLabel.Text = "USD($)"; // Localize
         }
 
         public async void OnCloseTapped(object sender, EventArgs e)
@@ -73,7 +61,62 @@ namespace HodlWallet2.Views
         {
             base.OnAppearing();
 
-            ProcessClipboardContent();
+            CreateInteractionBindings();
+            ViewModel.ProcessAddressOnClipboardToPaste();
+        }
+
+        void CreateInteractionBindings()
+        {
+            var set = this.CreateBindingSet<SendView, SendViewModel>();
+
+            set.Bind(this)
+                .For(view => view.QuestionInteraction)
+                .To(viewModel => viewModel.QuestionInteraction)
+                .OneWay();
+
+            set.Apply();
+        }
+
+        void SetLabels()
+        {
+            SendTitle.Text = LocaleResources.Send_title;
+            ToLabel.Text = LocaleResources.Send_to;
+            ScanLabel.Text = LocaleResources.Send_scan;
+            PasteLabel.Text = LocaleResources.Send_paste;
+            AmountLabel.Text = LocaleResources.Send_amount;
+            ISOLabel.Text = "USD($)"; // Localize
+        }
+
+        async void QuestionInteraction_Requested(object sender, MvxValueEventArgs<YesNoQuestion> e)
+        {
+            var yesNoQuestion = e.Value;
+
+            string title;
+            string content;
+            string yes = "Yes";
+            string no = "No";
+
+            switch (yesNoQuestion.QuestionKey)
+            {
+                case "use-address-on-clipboard":
+                    // FIXME we had to get the address again here...
+                    string address = await Clipboard.GetTextAsync();
+
+                    content = string.Format(
+                        LocaleResources.Send_addressDetectedOnClipboardMessage,
+                        address
+                    );
+                    title = LocaleResources.Send_addressDetectedOnClipboardTitle;
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid question sent, value: {yesNoQuestion.QuestionKey}");
+            }
+
+            bool answer = await DisplayAlert(
+                title, content, yes, cancel: no
+            );
+
+            yesNoQuestion.AnswerCallback(answer);
         }
     }
 }
