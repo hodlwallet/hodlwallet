0969550f749bf58ee67fc3f63132e5b3369bedd9
edgarnet <eaprmail@gmail.com>
2019-07-10 17:57:20 -0400

    Add TransactionDetails Text Binding for View Model

--- HodlWallet2.Core/ViewModels/TransactionDetailsViewModel.cs
+++ HodlWallet2.Core/ViewModels/TransactionDetailsViewModel.cs
@@ -24,6 +24,83 @@ namespace HodlWallet2.Core.ViewModels
         public string TransactionIdTitle => "Bitcoin Transaction ID";
         public string ConfirmedBlockTitle => "Confirmed in Block";
 
+        string _DateText;
+        public string DateText
+        {
+            get => _DateText;
+            set => SetProperty(ref _DateText, value);
+        }
+
+        string _AmountText;
+        public string AmountText
+        {
+            get => _AmountText;
+            set => SetProperty(ref _AmountText, value);
+        }
+
+        string _AddressToFromText;
+        public string AddressToFromText
+        {
+            get => _AddressToFromText;
+            set => SetProperty(ref _AmountText, value);
+        }
+
+        string _StatusText;
+        public string StatusText
+        {
+            get => _StatusText;
+            set => SetProperty(ref _StatusText, value);
+        }
+
+        string _MemoText;
+        public string MemoText
+        {
+            get => _MemoText;
+            set => SetProperty(ref _MemoText, value);
+        }
+
+        string _AmountWithFeeText;
+        public string AmountWithFeeText
+        {
+            get => _AmountWithFeeText;
+            set => SetProperty(ref _AmountWithFeeText, value);
+        }
+
+        string _StartingBalanceText;
+        public string StartingBalanceText
+        {
+            get => _StartingBalanceText;
+            set => SetProperty(ref _StartingBalanceText, value);
+        }
+
+        string _EndingBalanceText;
+        public string EndingBalanceText
+        {
+            get => _EndingBalanceText;
+            set => SetProperty(ref _EndingBalanceText, value);
+        }
+
+        string _AddressText;
+        public string AddressText
+        {
+            get => _AddressText;
+            set => SetProperty(ref _AddressText, value);
+        }
+
+        string _TransactionIdText;
+        public string TransactionIdText
+        {
+            get => _TransactionIdText;
+            set => SetProperty(ref _TransactionIdText, value);
+        }
+
+        string _ConfirmedBlockText;
+        public string ConfirmedBlockText
+        {
+            get => _ConfirmedBlockText;
+            set => SetProperty(ref _ConfirmedBlockText, value);
+        }
+
         public MvxAsyncCommand CloseCommand { get; }
         public MvxAsyncCommand ShowFaqCommand { get; }
 
