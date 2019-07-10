01f64ce20e529fe4e35d709b31ac67d765d79d01
edgarnet <eaprmail@gmail.com>
2019-07-10 17:03:31 -0400

    Setup TransactionDetails View Model

--- HodlWallet2.Core/ViewModels/TransactionDetailsViewModel.cs
+++ HodlWallet2.Core/ViewModels/TransactionDetailsViewModel.cs
@@ -1,22 +1,50 @@
 using System;
+using System.Threading.Tasks;
 
 using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 
+using HodlWallet2.Core.Models;
+
 using HodlWallet2.Core.Interfaces;
 
 namespace HodlWallet2.Core.ViewModels
 {
-    public class TransactionDetailsViewModel : BaseViewModel
+    public class TransactionDetailsViewModel : BaseViewModel<Transaction>
     {
+        readonly IWalletService _WalletService;
+        Transaction _Transaction;
+
+        public MvxAsyncCommand CloseCommand { get; }
+        public MvxAsyncCommand ShowFaqCommand { get; }
+
         public TransactionDetailsViewModel(
             IMvxLogProvider logProvider,
             IMvxNavigationService navigationService,
             IWalletService walletService) : base(logProvider, navigationService)
         {
+            _WalletService = walletService;
 
+            CloseCommand = new MvxAsyncCommand(Close);
+            ShowFaqCommand = new MvxAsyncCommand(ShowFaq);
+        }
+
+        public override void Prepare(Transaction parameter)
+        {
+            _Transaction = parameter;
+        }
+
+        Task ShowFaq()
+        {
+            //TODO: Implement FAQ
+            return Task.FromResult(this);
+        }
+
+        async Task Close()
+        {
+            await NavigationService.Close(this);
         }
     }
 }
