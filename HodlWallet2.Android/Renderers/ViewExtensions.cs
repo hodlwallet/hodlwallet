479a5d2dd377ca392ac53b21c36c41467850d1e6
edgarnet <eaprmail@gmail.com>
2019-07-22 18:36:03 -0400

    Add HideTabLabelsEffect for Android

---
+++ HodlWallet2.Android/Renderers/ViewExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Android.Views;
+
+namespace HodlWallet2.Droid.Renderers
+{
+    public static class ViewExtensions
+    {
+        public static List<View> RetrieveAllChildViews(this View view)
+        {
+            if (!(view is ViewGroup group))
+            {
+                return new List<View> { view };
+            }
+
+            var result = new List<View>();
+
+            for (var i = 0; i < group.ChildCount; i++)
+            {
+                var child = group.GetChildAt(i);
+
+                var childList = new List<View> { child };
+                childList.AddRange(RetrieveAllChildViews(child));
+
+                result.AddRange(childList);
+            }
+
+            return result.Distinct().ToList();
+        }
+    }
+}
