99ef029ee7754a336db3639ebcd53b11df37f587
edgarnet <eaprmail@gmail.com>
2019-08-16 20:01:48 -0400

    Start Legacy Secure Key Service

---
+++ HodlWallet2.Android/Services/LegacySecureKeyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Java.IO;
+using Javax.Crypto;
+
+namespace HodlWallet2.Droid.Services
+{
+    public class LegacySecureKeyService
+    {
+        public LegacySecureKeyService()
+        {
+        }
+    }
+}
---
+++ HodlWallet2.iOS/Services/LegacySecureKeyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace HodlWallet2.iOS.Services
+{
+    public class LegacySecureKeyService
+    {
+        public LegacySecureKeyService()
+        {
+        }
+    }
+}
