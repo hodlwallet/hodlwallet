531593bbb0e2e2f6f3147944c972a47d3f43137f
edgarnet <eaprmail@gmail.com>
2019-08-26 16:15:58 -0400

    Add Methods to ILegacySecureKeyService

---
+++ HodlWallet2/Core/Interfaces/ILegacySecureKeyService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HodlWallet2.Core.Interfaces
+{
+    public interface ILegacySecureKeyService
+    {
+        string GetMnemonic();
+
+        byte[] GetMasterPublicKey();
+
+        string GetPin();
+
+        long GetPinFailCount();
+
+        long GetSpendLimit();
+
+        byte[] GetApiAuthKey();
+
+        long GetWalletCreationTime();
+
+        long GetPinFailTime();
+    }
+}
