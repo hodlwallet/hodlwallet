9ed968438d08f59ff219af5a5fe9a5eabc12f97a
edgarnet <eaprmail@gmail.com>
2019-09-18 15:44:48 -0400

    [WIP] Start AccountsView and Update AccountCollectionView

---
+++ HodlWallet2/UI/Views/AccountsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HodlWallet2.UI.Views
+{
+    public partial class AccountsView : ContentPage
+    {
+        public AccountsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
