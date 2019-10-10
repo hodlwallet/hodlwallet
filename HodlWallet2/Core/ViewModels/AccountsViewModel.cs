d4ea184080610ee7b82a7e938eef09ded01b1d8a
edgarnet <eaprmail@gmail.com>
2019-10-10 13:33:36 -0400

    Add Bindable Properties to AccountsViewModel

--- HodlWallet2/Core/ViewModels/AccountsViewModel.cs
+++ HodlWallet2/Core/ViewModels/AccountsViewModel.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using HodlWallet2.Core.Models;
+
 namespace HodlWallet2.Core.ViewModels
 {
     public class AccountsViewModel : BaseViewModel
     {
+        public string AccountTitle => "Accounts";
+
+        string _PriceText;
+        object _CurrentAccount;
+
+        public string PriceText
+        {
+            get => _PriceText;
+            set => SetProperty(ref _PriceText, value);
+        }
+
+        public ObservableCollection<AccountModel> Accounts { get; } = new ObservableCollection<AccountModel>();
+
+        public object CurrentAccount
+        {
+            get => _CurrentAccount;
+            set => SetProperty(ref _CurrentAccount, value);
+        }
+
+        public ICommand NavigateToHomeCommand { get; }
+        public ICommand AddAccountCommand { get; }
+
         public AccountsViewModel()
         {
+            //Commands
         }
+
+        // Methods
     }
 }
