1fa3dd2c1bae1be20f10bcd2cb27bf261b8d2c41
edgarnet <eaprmail@gmail.com>
2019-10-10 15:50:40 -0400

    Update Account Model

--- HodlWallet2/Core/Models/AccountModel.cs
+++ HodlWallet2/Core/Models/AccountModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+using Xamarin.Forms;
+
 using Liviano.Interfaces;
 
 namespace HodlWallet2.Core.Models
 {
     public class AccountModel
     {
-        public IAccount Account { get; set; }
+        public IAccount Account { get; private set; }
+
+        public Color GradientStart { get; }
+
+        public Color GradientEnd { get; }
 
         public AccountModel(IAccount account)
         {
