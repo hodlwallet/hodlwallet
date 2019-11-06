73e7998f3ef56f72235a3b60d2fb7a0907733d5e
edgarnet <eaprmail@gmail.com>
2019-11-06 17:34:33 -0500

    Add Account Gradient to AccountModel

--- HodlWallet2/Core/Models/AccountModel.cs
+++ HodlWallet2/Core/Models/AccountModel.cs
@@ -20,31 +20,14 @@ namespace HodlWallet2.Core.Models
 
         public static AccountModel FromAccountData(IAccount account)
         {
-            var colors = RandomHex();
-
             return new AccountModel
             {
                 AccountData = account,
                 AccountName = account.Name,
                 Balance = account.GetBalance().ToString(),
-                GradientStart = Color.FromHex(colors.Item1),
-                GradientEnd = Color.FromHex(colors.Item2)
+                GradientStart = Color.FromHex(account.StartHex),
+                GradientEnd = Color.FromHex(account.EndHex)
             };
-        }
-
-        static (string, string) RandomHex()
-        {
-            // TODO Generate and store hex in wallet.
-            //      Add option for light or dark preference.
-
-            var rng = new Random();
-
-            var startRGB = (rng.Next(128, 200), rng.Next(128, 200), rng.Next(128, 200));
-
-            var endRGB = ((int)(startRGB.Item1 / 1.25), (int)(startRGB.Item2 / 1.25), (int)(startRGB.Item3 / 1.25));
-
-            return ($"{startRGB.Item1:X2}{startRGB.Item2:X2}{startRGB.Item3:X2}",
-                    $"{endRGB.Item1:X2}{endRGB.Item2:X2}{endRGB.Item3:X2}");
         }
     }
 }
