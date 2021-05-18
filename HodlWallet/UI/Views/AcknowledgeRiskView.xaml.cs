28507da1aad53ff0b8aaa69e60e8930218ecaa2b
marconipoveda <marconipoveda84@gmail.com>
2021-05-18 17:39:30 -0600

    feat: Added new views to show responsability info.
    Signed-off-by: marconipoveda <marconipoveda84@gmail.com>

---
+++ HodlWallet/UI/Views/AcknowledgeRiskView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using HodlWallet.UI.Locale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HodlWallet.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcknowledgeRiskView : ContentPage
+    {
+        public AcknowledgeRiskView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        void SetLabels()
+        {
+            //Title = LocaleResources.Backup_title;
+            AckTerm1.Text = LocaleResources.AckRisk_Term1;
+            AckTerm2.Text = LocaleResources.AckRisk_Term2;
+            AckTerm3.Text = LocaleResources.AckRisk_Term3;
+            AckTerm4.Text = LocaleResources.AckRisk_Term4;
+        }
+
+        void WalletInfoButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BackupRecoveryWordView());
+        }
+    }
+}
---
+++ HodlWallet/UI/Views/NewWalletInfoView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using HodlWallet.UI.Locale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HodlWallet.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewWalletInfoView : ContentPage
+    {
+        public NewWalletInfoView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        void SetLabels()
+        {
+            //Title = LocaleResources.Backup_title;
+            Header.Text = LocaleResources.NewWallet_header;
+            Subheader.Text = LocaleResources.NewWallet_subheader;
+            Button.Text = LocaleResources.NewWallet_button;
+        }
+
+        void WalletInfoButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AcknowledgeRiskView());
+        }
+    }
+}
