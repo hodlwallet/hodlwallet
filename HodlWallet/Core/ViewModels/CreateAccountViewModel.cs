63bc6098374daadfeead67aaa290ffde058071bf
ktorresno <ktorresno@gmail.com>
2021-05-26 16:36:25 -0600

    Draft: New View and logic to crete a new account in the current Wallet

---
+++ HodlWallet/Core/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+
+namespace HodlWallet.Core.ViewModels
+{
+    class CreateAccountViewModel : BaseViewModel
+    {
+        public ICommand CreateAccCommand { get; }
+
+        string _AccountName;
+
+        public string AccountName
+        {
+            get => _AccountName;
+            set => SetProperty(ref _AccountName, value);
+        }
+
+        string _AccountType;
+
+        public string AccountType
+        {
+            get => _AccountType;
+            set => SetProperty(ref _AccountType, value);
+        }
+        public CreateAccountViewModel()
+        {
+            CreateAccCommand = new Command(CreateAccount);
+        }
+        private async void CreateAccount()
+        {
+            Debug.WriteLine($"[CreateAcount Before] Trying to create a new account => Account Type: {AccountType} Account Name: {AccountName} ");
+            var (Success, Error) = WalletService.AddAccount(AccountType ?? "bip84", AccountName ?? "Karen's Bitcoin Account");
+            Debug.WriteLine($"[CreateAcount] Trying to create a new account => Account Type: {AccountType} Account Name: {AccountName} | Result => Success: {Success} Error: {Error} ");
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
---
+++ HodlWallet/UI/Views/CreateAccountView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using HodlWallet.UI.Locale;
+using HodlWallet.Core.Services;
+
+namespace HodlWallet.UI.Views
+{
+    public partial class CreateAccountView : ContentPage
+    {
+        public CreateAccountView()
+        {
+            InitializeComponent();
+            SetLabels();
+        }
+
+        void SetLabels()
+        {
+            Title = LocaleResources.AddAccount_title;
+            Header.Text = LocaleResources.AddAccount_header;
+            //Subheader.Text = LocaleResources.AddAccount_subheader;
+            AddAccountLbl.Text = LocaleResources.AddAccountText_label;
+            CreateAcc.Text = LocaleResources.AddAccount_button;
+        }
+        
+    }
+}
