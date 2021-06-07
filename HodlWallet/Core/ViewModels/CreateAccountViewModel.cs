292125c0a4708f82d7476c1c0659644c761e7351
ktorresno <ktorresno@gmail.com>
2021-06-07 15:46:46 -0600

    Fix: Following C# naming convention  on local variables. Avoiding bad abreviation name for bindable command "CreateAccountCommand".

--- HodlWallet/Core/ViewModels/CreateAccountViewModel.cs
+++ HodlWallet/Core/ViewModels/CreateAccountViewModel.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿//
+// CreateAccountViewModel.cs
+//
+// Copyright (c) 2019 HODL Wallet
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Input;
 
@@ -9,32 +32,32 @@ namespace HodlWallet.Core.ViewModels
 {
     class CreateAccountViewModel : BaseViewModel
     {
-        public ICommand CreateAccCommand { get; }
+        string[] accountTypesList = { "bip84", "bip141" };
+        public IList<string> AcountTypes { get => accountTypesList; }
+        public ICommand CreateAccountCommand { get; }
 
-        string _AccountName;
+        string accountName;
 
         public string AccountName
         {
-            get => _AccountName;
-            set => SetProperty(ref _AccountName, value);
+            get => accountName;
+            set => SetProperty(ref accountName, value);
         }
 
-        string _AccountType;
+        string accountType;
 
         public string AccountType
         {
-            get => _AccountType;
-            set => SetProperty(ref _AccountType, value);
+            get => accountType;
+            set => SetProperty(ref accountType, value);
         }
         public CreateAccountViewModel()
         {
-            CreateAccCommand = new Command(CreateAccount);
+            CreateAccountCommand = new Command(CreateAccount);
         }
         private async void CreateAccount()
         {
-            Debug.WriteLine($"[CreateAcount Before] Trying to create a new account => Account Type: {AccountType} Account Name: {AccountName} ");
-            var (Success, Error) = await WalletService.AddAccount(AccountType ?? "bip84", AccountName ?? "Karen's Bitcoin Account");
-            Debug.WriteLine($"[CreateAcount] Trying to create a new account => Account Type: {AccountType} Account Name: {AccountName} | Result => Success: {Success} Error: {Error} ");
+            var (Success, Error) = await WalletService.AddAccount(AccountType ?? accountTypesList[0], AccountName);
             await Shell.Current.GoToAsync("..");
         }
     }
