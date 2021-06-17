1ecaf026e02959fff337822b62bec6b35a7c610d
Jonathan <77517179+Jonathan-G555@users.noreply.github.com>
2021-06-16 19:58:02 -0600

    ColorPicker and DemoView Added

---
+++ HodlWallet/UI/Views/Demos/ColorPickerDemoView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HodlWallet.UI.Views.Demos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColorPickerDemoView : ContentPage
+    {
+        public ColorPickerDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
