6ed1c29c84ce60b320820f40ee02303a8a4691bd
Igor Guerrero <igorgue@protonmail.com>
2021-09-28 11:45:36 -0600

    Add account info view for now an empty view

--- HodlWallet/UI/Controls/SecretTextContentView.xaml.cs
+++ HodlWallet/UI/Controls/SecretTextContentView.xaml.cs
@@ -1,5 +1,5 @@
 ﻿//
-// SyncingToolbarItem.xaml.cs
+// SecretTextContentView.xaml.cs
 //
 // Author:
 //       Igor Guerrero <igorgue@protonmail.com>
@@ -23,48 +23,17 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace HodlWallet.UI.Controls
 {
-    public partial class SyncingToolbarItem : HideableToolbarItem
+    public partial class SecretTextContentView : ContentView
     {
-        readonly List<string> states = new () { "node", "node-0-connections", "node-1-connections", "node-2-connections", "node-3-connections" };
-        readonly CancellationTokenSource Cts;
-
-        public SyncingToolbarItem()
+        public SecretTextContentView()
         {
             InitializeComponent();
-
-            Cts = new CancellationTokenSource();
-
-            Task.Factory.StartNew(
-                () => DoFlip(),
-                Cts.Token,
-                TaskCreationOptions.LongRunning,
-                TaskScheduler.Default
-            );
-        }
-
-        async void DoFlip()
-        {
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                var fileName = (IconImageSource as FileImageSource).File;
-                var index = states.IndexOf(fileName);
-
-                var newFileName = states[++index % 5];
-
-                IconImageSource = newFileName;
-            });
-
-            await Task.Delay(1_000);
-
-            DoFlip();
         }
     }
 }
