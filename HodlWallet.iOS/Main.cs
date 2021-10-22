8d27cf8dcb0dc2d5977bbe6b81514f4e16794e00
Igor Guerrero <igorgue@protonmail.com>
2021-10-21 22:28:11 -0600

    Fix deprecated call

--- HodlWallet.iOS/Main.cs
+++ HodlWallet.iOS/Main.cs
@@ -31,7 +31,7 @@ namespace HodlWallet.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
