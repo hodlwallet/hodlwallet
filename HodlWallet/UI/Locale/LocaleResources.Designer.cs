3c7534acb3aaa2dfdf48c5264c4bcfb4e29e71ce
Jonathan <jonathan.garcia.lopez.a@gmail.com>
2021-11-25 00:28:04 -0600

    Erasing Login.cancel of LocaleResoulces

--- HodlWallet/UI/Locale/LocaleResources.Designer.cs
+++ HodlWallet/UI/Locale/LocaleResources.Designer.cs
@@ -19,7 +19,7 @@ namespace HodlWallet.UI.Locale {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LocaleResources {
@@ -669,15 +669,6 @@ namespace HodlWallet.UI.Locale {
         internal static string FingerprintAuth_subheader {
             get {
                 return ResourceManager.GetString("FingerprintAuth.subheader", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Cancel.
-        /// </summary>
-        internal static string Login_cancel {
-            get {
-                return ResourceManager.GetString("Login.cancel", resourceCulture);
             }
         }
         
