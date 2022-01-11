c76cb5798dd3f13023a26bd3178708d81c0a5305
Jonathan <jonathan.garcia.lopez.a@gmail.com>
2022-01-10 20:21:05 -0600

    Adding CloseToolbarItem, and changing the way of opening the next view on PinPadView

--- HodlWallet/UI/Views/PinPadView.xaml.cs
+++ HodlWallet/UI/Views/PinPadView.xaml.cs
@@ -33,16 +33,27 @@ namespace HodlWallet.UI.Views
 {
     public partial class PinPadView : ContentPage
     {
-        readonly ContentPage nextView;
-
-        public PinPadView(ContentPage nextView)
+        string nextView;
+        public PinPadView(string nextView)
         {
-            this.nextView = nextView;
-
             InitializeComponent();
-
             SetPinPadControlBindings();
             SubscribeToMessages();
+
+            this.nextView = nextView;
+            if (this.nextView == "PinPadChangeView")
+            {
+                CloseToolbarItem.IsEnabled = true;
+            }
+            else
+            {
+                CloseToolbarItem.IsEnabled = false;
+            }
+        }
+
+        void CloseToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
         }
 
         void SetPinPadControlBindings()
@@ -63,7 +74,20 @@ namespace HodlWallet.UI.Views
 
         async void NavigateToNextView(PinPadViewModel _)
         {
-            await Navigation.PushAsync(nextView);
+            switch(nextView)
+            {
+                case "NewWalletInfoView":
+                    await Navigation.PushAsync(new NewWalletInfoView());
+                    break;
+
+                case "PinPadChangeView":
+                    await Navigation.PushAsync(new PinPadChangeView());
+                    break;
+
+                case "RecoverInfoView":
+                    await Navigation.PushAsync(new RecoverInfoView());
+                    break;
+            }
 
             // No page should go back to the pinpad view
             if (Navigation.NavigationStack.Contains(this)) Navigation.RemovePage(this);
