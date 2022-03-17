4afe0a5950c4a99e2c4f13bcd5206e8465827ecc
Igor Guerrero <igorgue@protonmail.com>
2022-03-16 20:03:02 -0600

    Add switches to prevent clicking on them twice

--- HodlWallet/UI/Views/MyBitcoinView.xaml.cs
+++ HodlWallet/UI/Views/MyBitcoinView.xaml.cs
@@ -12,6 +12,9 @@ namespace HodlWallet.UI.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MyBitcoinView : ContentPage
     {
+        bool isOpeningSend = false;
+        bool isOpeningReceive = false;
+
         public MyBitcoinView()
         {
             InitializeComponent();
@@ -19,12 +22,20 @@ namespace HodlWallet.UI.Views
 
         async void SendBitcoin_Tapped(object sender, EventArgs e)
         {
+            if (isOpeningSend) return;
+            isOpeningSend = true;
+
             await Navigation.PushAsync(new SendView());
+            isOpeningSend = false;
         }
 
         async void ReceiveBitcoin_Tapped(object sender, EventArgs e)
         {
+            if (isOpeningReceive) return;
+            isOpeningReceive = true;
+
             await Navigation.PushAsync(new ReceiveView());
+            isOpeningReceive = false;
         }
     }
 }
