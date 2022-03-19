c164e8f74a62f11cfef201a34851128c06e417fe
Igor Guerrero <igorgue@protonmail.com>
2022-03-18 18:45:58 -0600

    Make a balance header control so we can attach it multiple times

---
+++ HodlWallet/Core/ViewModels/BalanceHeaderViewModel.cs
@@ -0,0 +1,83 @@
+﻿//
+// BalanceHeaderViewModel.cs
+//
+// Copyright (c) 2022 HODL Wallet
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Specialized;
+
+using NBitcoin;
+
+using Liviano.Interfaces;
+
+namespace HodlWallet.Core.ViewModels
+{
+    class BalanceHeaderViewModel : BaseViewModel
+    {
+        string balance;
+        public string Balance
+        {
+            get => balance;
+            set => SetProperty(ref balance, value);
+        }
+
+        string balanceFiat;
+        public string BalanceFiat
+        {
+            get => balanceFiat;
+            set => SetProperty(ref balanceFiat, value);
+        }
+
+        IAccount CurrentAccount => WalletService.Wallet.CurrentAccount;
+
+        public BalanceHeaderViewModel()
+        {
+            if (WalletService.IsStarted)
+            {
+                UpdateBalanceLabels();
+                CurrentAccount.Txs.CollectionChanged += Txs_CollectionChanged;
+            }
+            else WalletService.OnStarted += WalletService_OnStarted;
+        }
+
+        void Txs_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateBalanceLabels();
+        }
+
+        void WalletService_OnStarted(object sender, EventArgs e)
+        {
+            UpdateBalanceLabels();
+
+            CurrentAccount.Txs.CollectionChanged += Txs_CollectionChanged;
+        }
+
+        void UpdateBalanceLabels()
+        {
+            var accBalance = CurrentAccount.GetBalance();
+            Balance = $"{accBalance} BTC";
+
+            var accBalanceFiat = ((decimal)PrecioService.Rate.Rate) * accBalance.ToDecimal(MoneyUnit.BTC);
+
+            if (PrecioService.Rate.Code == "USD") BalanceFiat = accBalanceFiat.ToString("C");
+            else balanceFiat = $"{PrecioService.Rate.Code} {accBalanceFiat}";
+        }
+    }
+}
