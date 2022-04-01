a30fcfe805c6951f96e3ce4b467a5da10d831ee8
Igor Guerrero <igorgue@protonmail.com>
2022-04-01 17:43:56 -0600

    Fix colors on toast

--- HodlWallet/UI/Extensions/ContentPageExtentions.cs
+++ HodlWallet/UI/Extensions/ContentPageExtentions.cs
@@ -46,8 +46,8 @@ namespace HodlWallet.UI.Extensions
             Guard.NotEmpty(message, nameof(message));
 
             var res = Application.Current.Resources;
-            var bg = (Color)res["Bg3"];
-            var fg = (Color)res["Fg"];
+            var bg = (Color)res["Bg2"];
+            var fg = (Color)res["Fg2"];
             var fontFamily = (string)res["Sans-Regular"];
             var font = Font.OfSize(fontFamily, Device.GetNamedSize(NamedSize.Medium, view));
 
