9f6bce68abad3062e7e6534c328b2b6579eed69a
Igor Guerrero <igorgue@protonmail.com>
2022-04-05 19:08:38 -0600

    Fix title view on all platforms, technical dificulties doesnt allow it to be centered on iOS so we are doing start.

--- HodlWallet/UI/Controls/TitleView.xaml.cs
+++ HodlWallet/UI/Controls/TitleView.xaml.cs
@@ -23,6 +23,9 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.using Xamarin.Forms;
+using System;
+using System.Linq;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -34,6 +37,62 @@ namespace HodlWallet.UI.Controls
         public TitleView()
         {
             InitializeComponent();
+        }
+
+        void AddNeededMarginToCenterInIOS()
+        {
+            var view = Parent as ContentPage;
+
+            view.Appearing += ParentContentPage_Appearing;
+        }
+
+        private void ParentContentPage_Appearing(object sender, EventArgs e)
+        {
+            var view = sender as ContentPage;
+            var hasNavigationPage = NavigationPage.GetHasNavigationBar(view);
+            var hasNavigationBackButton = NavigationPage.GetHasBackButton(view);
+
+            // If we do not have a navigation then we don't need to adjust anything
+            if (!hasNavigationPage) return;
+
+            var items = view.ToolbarItems.Where(item =>
+            {
+                if (item is HideableToolbarItem)
+                    return (item as HideableToolbarItem).IsVisible;
+
+                return item.IsEnabled;
+            }).ToArray();
+            var itemLen = items.Length;
+
+            // If it's a shell item, it's on a shell, and will have a menu
+            var hasMenu = view.Parent is BaseShellItem;
+
+            // has menu and on the other side an item
+            // center is enough
+            if (hasMenu && itemLen == 1) return;
+
+            var left = 0;
+            var right = -(40 * itemLen);
+            Thickness thickness;
+            if (hasMenu || hasNavigationBackButton)
+            {
+                left = -40;
+            }
+            else
+            {
+                left = 0;
+            }
+            thickness = new Thickness(left, 0, right, 0);
+
+            titleLabel.Margin = thickness;
+        }
+
+        protected override void OnParentSet()
+        {
+            // TODO This could make them center on iOS like I explain
+            // in xaml
+            //if (Device.RuntimePlatform == Device.iOS)
+            //    AddNeededMarginToCenterInIOS();
         }
     }
 }
